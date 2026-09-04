06d47fa032c84c11368fb8de708111d373308a4a
Danielson1234567 <silva.gomes.daniel@escola.pr.gov.br>
2026-09-04 13:03:59 -0300

    a

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 const botoes = document . queryselectorA11(".botao");
-console.log(botoes);
+
+for(i=0;i <botoes .length;i++){
+  botoes[i].onclik = funtion(){
+
+    for(let j=0;botoes.length;j++){
+        botoes[j].classlist.remove("ativo")
+    }
+
+    botoes[i].classlist.add("ativo");
+  }
+    console.log();
+}
