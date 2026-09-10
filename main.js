2a9ea8bc4ec28c7af49d57ede3167b5329377cbe
Danielson1234567 <silva.gomes.daniel@escola.pr.gov.br>
2026-09-10 11:49:03 -0300

    a

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 const botoes = document.querySelectorAll(".botao");
 
 botoes.forEach((botao) => {
-  botao.onclick = function () {
+  botao.addEventListener("click", () => {
     // Remove a classe "ativo" de todos os botões
     botoes.forEach((b) => b.classList.remove("ativo"));
-    
+
     // Adiciona a classe "ativo" apenas no botão clicado
     botao.classList.add("ativo");
-  };
+  });
 });
